ed6bee460d953c945b95d0cae0f5bab327f8bdfa
Tyler Thomas <TylerEdwardThomas@gmail.com>
2024-03-09 16:56:51 -0500

    add resize function to windows

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Make elements draggable
 function dragElement(element) {
-  var pos1 = 0,
+  let pos1 = 0,
     pos2 = 0,
     pos3 = 0,
     pos4 = 0;
@@ -18,7 +18,7 @@ function dragElement(element) {
     //This takes the current position of the mouse/widow, so that we can compare it to the new position once dragged
     pos3 = e.clientX;
     pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
+    document.onmouseup = removeMouseEvents;
     document.onmousemove = elementDrag;
   }
 
@@ -33,11 +33,56 @@ function dragElement(element) {
     element.style.left = element.offsetLeft - pos1 + "px";
   }
 
-  function closeDragElement() {
-    // remves the event listener when user stops dragging element
+  function removeMouseEvents() {
+    // remves the event listener when user stops dragging/resizing element
     document.onmouseup = null;
     document.onmousemove = null;
   }
+
+  if (document.getElementById(element.id + "-resize-handle")) {
+    // gets the resize div and adds event if it exists
+    let resizeHandle = document.getElementById(element.id + "-resize-handle");
+    resizeHandle.onmousedown = resizeMouseDown;
+  }
+
+  function resizeMouseDown(e) {
+    e.preventDefault();
+    // Gets starting positions
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = removeMouseEvents;
+    document.onmousemove = elementResize;
+  }
+
+  function elementResize(e) {
+    e.preventDefault();
+    // Calculate new sizes
+    let pos1 = pos3 - e.clientX;
+    let pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    var newWidth = element.offsetWidth - pos1;
+    var newHeight = element.offsetHeight - pos2;
+    // Windows were able to be resized off screen, this checks if the new size compared to the screen size
+    let maxWidth = window.innerWidth - element.offsetLeft - 4;
+    let taskbarHeight = document.getElementById("taskbar").offsetHeight;
+    let maxHeight = window.innerHeight - element.offsetTop - taskbarHeight;
+    if (newWidth > maxWidth) {
+      newWidth = maxWidth;
+    }
+    if (newHeight > maxHeight) {
+      newHeight = maxHeight;
+    }
+    // checks for a minimum so they can't resize the window too small to interact with
+    if (newWidth < 100) newWidth = 100;
+    if (newHeight < 50) newHeight = 50;
+
+    // Apply new sizes to element
+    element.style.width = newWidth + "px";
+    element.style.height = newHeight + "px";
+  }
+
+  // TODO prevent moving a window offscreen from stretching the page
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -46,20 +91,11 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function openWindow(id) {
-  var element = document.getElementById(id);
+  let element = document.getElementById(id);
   element.style.display = "block";
   dragElement(element);
 }
 
 function closeWindow(id) {
   document.getElementById(id).style.display = "none";
-}
-
-function resizeMouseDown(e) {
-  e.preventDefault();
-  // Initialize starting position
-  pos3 = e.clientX;
-  pos4 = e.clientY;
-  document.onmouseup = stopResize;
-  document.onmousemove = elementResize;
 }
