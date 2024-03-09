a3e3c162e8920cef968fff40bb6a621fe2f6b6aa
Tyler Thomas <TylerEdwardThomas@gmail.com>
2024-03-09 16:17:55 -0500

    Updated comments, adding iframes to windows

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // Make elements draggable
-function dragElement(elmnt) {
+function dragElement(element) {
   var pos1 = 0,
     pos2 = 0,
     pos3 = 0,
     pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  //If the window has a child with the titlebar id, it will only allow dragging from the titlebar.
+  if (document.getElementById(element.id + "-title-bar")) {
+    document.getElementById(element.id + "-title-bar").onmousedown =
+      dragMouseDown;
   } else {
-    elmnt.onmousedown = dragMouseDown;
+    //Else just make the whole window draggable.
+    element.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
-    e = e || window.event;
     e.preventDefault();
+    //This takes the current position of the mouse/widow, so that we can compare it to the new position once dragged
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
@@ -20,17 +23,18 @@ function dragElement(elmnt) {
   }
 
   function elementDrag(e) {
-    e = e || window.event;
     e.preventDefault();
+    //Calculates the new position of the element and adjusts the element to that
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    element.style.top = element.offsetTop - pos2 + "px";
+    element.style.left = element.offsetLeft - pos1 + "px";
   }
 
   function closeDragElement() {
+    // remves the event listener when user stops dragging element
     document.onmouseup = null;
     document.onmousemove = null;
   }
@@ -49,4 +53,13 @@ function openWindow(id) {
 
 function closeWindow(id) {
   document.getElementById(id).style.display = "none";
+}
+
+function resizeMouseDown(e) {
+  e.preventDefault();
+  // Initialize starting position
+  pos3 = e.clientX;
+  pos4 = e.clientY;
+  document.onmouseup = stopResize;
+  document.onmousemove = elementResize;
 }
